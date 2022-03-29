b7c6d0cb14b44472de8c21263eef3018aacaf5c2
Zul Hilmi <netspytux@gmail.com>
2022-03-29 15:39:03 +0700

    mengubah menu navigasi

--- src/components/Navigasi.vue
+++ src/components/Navigasi.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col-md-12">
         <nav class="nav pt-3 pb-3 justify-content-center">
-          <router-link to="/" class="nav-link">home</router-link>
+          <router-link to="/" class="nav-link">beranda</router-link>
           <router-link to="/products" class="nav-link">products</router-link>
           <router-link to="/contact" class="nav-link">contact</router-link>
         </nav>
